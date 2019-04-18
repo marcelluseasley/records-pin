24ae08b327c2f617124850c505d3ba0e385fed7a
Marcellus Easley <marcellus.easley@gmail.com>
2019-04-18 08:00:59 +0100

    set up router and routes

---
+++ server/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+
+	
+	_ "github.com/mattn/go-sqlite3"
+
+)
+
+// Record represents call data that will be stored in the database
+type Record struct {
+	PhoneNumber string  `json:"phone_number"`
+	Carrier     string  `json:"carrier"`
+	Score       float64 `json:"score"`
+}
+
+
+
+func main() {
+	createdatabase()
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	router.Route("/v1/record", func(r chi.Router) {
+		r.Post("/create", createRecordHandler)
+		r.Post("/read", readRecordHandler)
+		r.Put("/update", updateRecordHandler)
+		r.Delete("/delete", deleteRecordHandler) //TODO: add authentication
+	})
+
+	router.Route("/v1/records", func(r chi.Router) {
+		r.Post("/listall", listAllHandler)
+		r.Delete("/deleteall", deleteAllHandler) //TODO: add authentication
+
+	})
+
+	http.ListenAndServe(":8080", router)
+}
+
+
